f4e8d7b62db27940b17b88f8f007740b65d70524
abhishekkushwaha0725-netizen <abhishekkushwaha0725@gmail.com>
2026-07-05 18:44:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let display = document.getElementById("display");
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+function toggleMode() {
+  document.body.classList.toggle("dark");
+}
+document.addEventListener("keydown", function(e) {
+  if (!isNaN(e.key) || "+-*/.".includes(e.key)) {
+    appendValue(e.key);
+  } else if (e.key === "Enter") {
+    calculate();
+  } else if (e.key === "Backspace") {
+    backspace();
+  }
+});
